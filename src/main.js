e2ae02c6509b2f824c3fefb9f427d1beabae2616
ykbolo <49478987+ykbolo@users.noreply.github.com>
2019-09-25 10:49:25 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,8 +1,26 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
+import store from './store'
+
+import './core/boot'
+
+import 'bootstrap'
+import './styles/global.scss'
+
+// global components
+import './components'
+
+// global directives
+import './directives'
+
+import i18n from './i18n'
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  router,
+  store,
+  i18n,
+  render: h => h(App)
 }).$mount('#app')
