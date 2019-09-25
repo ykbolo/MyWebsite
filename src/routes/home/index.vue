6cf6704b3975e2710d90c61cc27f3f2cdfe088f6
kang_yang <kang_yang@berta.com>
2019-09-25 17:23:01 +0800

    首页信息

--- src/routes/home/index.vue
+++ src/routes/home/index.vue
@@ -3,13 +3,13 @@
     <div class="text-center">
       <h1>首页</h1>
       <h2>
-        <router-link :to="{name: 'color-pick'}">Examples</router-link>
+        <router-link :to="{name: 'color-pick'}">代码天地</router-link>
       </h2>
       <h2>
-        <router-link :to="{name: 'about-me'}">Login</router-link>
+        <router-link :to="{name: 'about-me'}">关于我</router-link>
       </h2>
       <h2>
-        <router-link :to="{name: 'more'}">userInfo</router-link>
+        <router-link :to="{name: 'more'}">文章</router-link>
       </h2>
     </div>
   </div>
