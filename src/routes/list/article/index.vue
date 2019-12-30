c31242e98e30d50ffca57d4d6b9e3e86296a62ee
503589510YK <503589510@qq.com>
2019-12-30 23:59:45 +0800

    第一篇文章

--- src/routes/list/article/index.vue
+++ src/routes/list/article/index.vue
@@ -1,22 +1,27 @@
 <template>
   <div class="aritcle-list">
-    <div class="every margin-t-1x">
-      <div class="every-head">
-        <span>第一：开放源码</span>
-        <br></div>
-      <div class="every-body">vue项目的架构思路以及引入包</div>
+    <div v-if="showlist">
+      <div @click="showcontent(1)" class="every margin-t-1x">
+        <div class="every-head">
+          <span>第一：git的使用</span>
+          <br></div>
+        <div class="every-body">用git对于代码进行版本控制</div>
+      </div>
+      <div @click="showcontent(2)" class="every">
+        <div class="every-head">
+          <span>第二：d3深入浅出</span>
+          <br></div>
+        <div class="every-body">基于d3的可视化研究</div>
+      </div>
+      <div @click="showcontent(3)" class="every">
+        <div class="every-head">
+          <span>第三：git的使用</span>
+          <br></div>
+        <div class="every-body">小白也能会的git命令</div>
+      </div>
     </div>
-    <div class="every">
-      <div class="every-head">
-        <span>第二：d3深入浅出</span>
-        <br></div>
-      <div class="every-body">基于d3的可视化研究</div>
-    </div>
-    <div class="every">
-      <div class="every-head">
-        <span>第三：git的使用</span>
-        <br></div>
-      <div class="every-body">小白也能会的git命令</div>
+    <div v-else>
+      <app-content :id="id" type='article'></app-content>
     </div>
   </div>
 </template>
