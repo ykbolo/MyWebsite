cf97d81f8a05fef0fffcee86c57dac019631d0a5
yangkang <yangkang@bertadata.com>
2020-01-03 15:43:07 +0800

    文章图片资源

--- src/routes/list/article/index.vue
+++ src/routes/list/article/index.vue
@@ -7,12 +7,18 @@
           <br></div>
         <div class="every-body">用git对于代码进行版本控制</div>
       </div>
-      <!-- <div @click="showcontent(2)" class="every">
+      <div @click="showcontent(2)" class="every margin-t-1x">
+        <div class="every-head">
+          <span>第二：vue+node+nginx配置gzip</span>
+          <br></div>
+        <div class="every-body">提升70%的无缓存情况下的访问速度</div>
+      </div>
+      <!--<div @click="showcontent(2)" class="every">
         <div class="every-head">
           <span>第二：d3深入浅出</span>
           <br></div>
         <div class="every-body">基于d3的可视化研究</div>
-      </div>
+      </div> 
       <div @click="showcontent(3)" class="every">
         <div class="every-head">
           <span>第三：git的使用</span>
